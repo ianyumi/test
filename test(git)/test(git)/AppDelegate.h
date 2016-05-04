19268bb378b2a93d5fb9224f36de58f07f412b93
ianyumi <iamyumi@163.com>
2016-05-04 19:39:48 +0800

    初始化项目

---
+++ test(git)/test(git)/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test(git)
+//
+//  Created by ianyumi on 16/5/4.
+//  Copyright © 2016年 ianyumi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test(git)/test(git)/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  test(git)
+//
+//  Created by ianyumi on 16/5/4.
+//  Copyright © 2016年 ianyumi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
